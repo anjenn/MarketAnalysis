ebf0da8dd589da0f92151d0b9404a169a83c004a
Jenn An <anjenn9912@gmail.com>
2025-03-06 18:21:40 +0900

    model_fine_tuning

--- model.py
+++ model.py
@@ -4,12 +4,12 @@ from sklearn.preprocessing import MinMaxScaler, StandardScaler
 import tensorflow as tf
 import matplotlib.pyplot as plt
 import joblib
+from sklearn.model_selection import cross_val_score
 from tensorflow.keras import layers, models
 from sklearn.model_selection import train_test_split
 from tensorflow.keras.regularizers import l2
 from tensorflow.keras.optimizers import Adam
 from tensorflow.keras.callbacks import EarlyStopping
-# from sklearn.model_selection import cross_val_score
 import utils
 
 from sklearn.model_selection import train_test_split
@@ -24,7 +24,7 @@ cleaned_data = [{k: v for k, v in item.items() if k in required_keys} for item i
 df = pd.DataFrame(cleaned_data)
 
 # REMOVING OUTLIERS
-bins = np.linspace(df['QUANTITY'].min(), df['QUANTITY'].max(), num=10)  # 10 bins
+bins = np.linspace(df['QUANTITY'].min(), df['QUANTITY'].max(), num=7)  # 7 bins
 labels = [f"Range {i}" for i in range(len(bins)-1)]  # Labels for each range
 df['QUANTITY_RANGE'] = pd.cut(df['QUANTITY'], bins=bins, labels=labels, include_lowest=True)
 price_median = df.groupby('QUANTITY_RANGE')['UNIT_PRICE'].median().reset_index()
@@ -46,16 +46,24 @@ X_raw = df_filtered[["ITEM_COUNT", "QUANTITY", "REVIEW_RATIO", "REVIEW_COUNT", "
 y = df_filtered["UNIT_PRICE"].values  # Predict total price
 
 # DATA TILING
+
 X_tiled = np.tile(X_raw, (3, 1))  # Duplicate the features
 y_tiled = np.tile(y, 3)  # Duplicate the target variable
 
-X_train, X_val, y_train, y_val = train_test_split(X_raw, y, test_size=0.2, random_state=42)
+# Training data - used to train the model
+# Validation data - used to test the model and adjust hyperparameters during training
+# Test data - used unviased performance estimate when training is done.
+X_train, X_temp, y_train, y_temp = train_test_split(X_raw, y, test_size=0.2, random_state=42)
+# X_train, X_val, y_train, y_val = train_test_split(X_raw, y, test_size=0.2, random_state=42)
+X_val, X_test, y_val, y_test = train_test_split(X_temp, y_temp, test_size=0.5, random_state=42) #  validation (10%) and test (10%)
+
 
 # Normalization
 scaler_X = MinMaxScaler()
 X_train_scaled = scaler_X.fit_transform(X_train)
 X_val_scaled = scaler_X.transform(X_val)
-y_log = np.log1p(y_train)  # Log transformation for training target
+X_test_scaled = scaler_X.transform(X_test)
+y_train_log = np.log1p(y_train)  # Log transformation for training target
 y_val_log = np.log1p(y_val)  # Log transformation for validation target
 
 joblib.dump(scaler_X, 'scaler_x.pkl')  # Save the scaler for future use
@@ -81,7 +89,7 @@ joblib.dump(scaler_X, 'scaler_x.pkl')  # Save the scaler for future use
 # model.compile(optimizer=Adam(learning_rate=0.0005), loss="mae")
 # early_stop = EarlyStopping(monitor="val_loss", patience=10, restore_best_weights=True)
 # model.fit(
-#     X_train_scaled, y_log,
+#     X_train_scaled, y_train_log,
 #     epochs=100, batch_size=16,
 #     validation_data=(X_val_scaled, y_val_log),
 #     callbacks=[early_stop]) # Adjust epochs, batch_size, and validation_split as needed
@@ -97,18 +105,17 @@ joblib.dump(scaler_X, 'scaler_x.pkl')  # Save the scaler for future use
 # # XGBoost Model
 
 import xgboost as xgb
-from xgboost import cv
 
 model = xgb.XGBRegressor(
-    n_estimators=2500,  # Number of boosting rounds
-    learning_rate=0.001,  # Learning rate
-    max_depth=8,  # Maximum depth of trees
-    min_child_weight=1,  # Minimum sum of instance weight (hessian) needed in a child
+    n_estimators=2000,  # Number of boosting rounds
+    learning_rate=0.0025,  # Learning rate
+    max_depth=4,  # Maximum depth of trees
+    min_child_weight=3,  # Minimum sum of instance weight (hessian) needed in a child
     subsample=0.7,  # Fraction of samples used in each boosting round
     colsample_bytree=0.7,  # Fraction of features used for each boosting round
     objective='reg:squarederror',  # Regression objective
     n_jobs=-1,  # Use all available CPU threads
-    reg_alpha=0.0,  # Remove L1 regularization
+    reg_alpha=0.2,  # Remove L1 regularization
     reg_lambda=0.5,  # Remove L2 regularization
     eval_metric='rmse',  # Track RMSE during training
     early_stopping_rounds=100
@@ -117,51 +124,69 @@ model = xgb.XGBRegressor(
 # Train the model
 model.fit(
     X_train_scaled,
-    y_log,
-    eval_set=[(X_train_scaled, y_log)],
+    y_train_log,
+    eval_set=[(X_train_scaled, y_train_log), (X_val_scaled, y_val_log)],
+    verbose=True  # Print the evaluation results at each iteration
 )
 
+print(f"Best iteration: {model.best_iteration}")
+
 params = {
-    'n_estimators': 2500,  # Regression objective
-    'learning_rate': 0.005,  # Root mean square error for evaluation
-    'max_depth': 8,  # Maximum depth of trees
-    'min_child_weight': 1,  # Learning rate
+    # 'n_estimators': 2500,  # Regression objective
+    'learning_rate': 0.0025,  # Root mean square error for evaluation
+    'max_depth': 4,  # Maximum depth of trees
+    'min_child_weight': 3,  # Learning rate
     'subsample': 0.7,  # Fraction of data to use for each tree
     'colsample_bytree': 0.7,  # Fraction of features to use for each tree
     'objective':'reg:squarederror',  # Regression objective
     'n_jobs': -1,  # Use all available CPU threads
-    'reg_alpha': 0.0,  # Remove L1 regularization
+    'reg_alpha': 0.2,  # Remove L1 regularization
     'reg_lambda': 0.5, # Remove L2 regularization
     'eval_metric': 'rmse',  # Track RMSE during training
+    # 'early_stopping_rounds': 100
 }
 
-dtrain = xgb.DMatrix(X_train_scaled, label=y_log)
+dtrain = xgb.DMatrix(X_train_scaled, label=y_train_log)
 
 cv_results = xgb.cv(
     params=params,
     dtrain=dtrain,
     num_boost_round=5000,
     early_stopping_rounds=100,
-    metrics=["rmse"],
+    nfold=3,
     seed=42  # For reproducibility
 )
+
+# Extract RMSE values from the cv_results DataFrame
+train_rmse = cv_results['train-rmse-mean']
+test_rmse = cv_results['test-rmse-mean']
+
+# Calculate RMSE scores for both train and test sets
+train_rmse_scores = np.sqrt(train_rmse)
+test_rmse_scores = np.sqrt(test_rmse)
+
+# Calculate the mean and standard deviation of RMSE scores for the test set
+mean_rmse = np.mean(test_rmse_scores)
+std_rmse = np.std(test_rmse_scores)
+
+# Print RMSE scores, mean, and standard deviation
+print(f"Cross-validation Test RMSE Scores: {test_rmse_scores}")
+print(f"Mean RMSE: {mean_rmse}")
+print(f"Standard Deviation of RMSE: {std_rmse}")
 
 cv_results[['train-rmse-mean', 'test-rmse-mean']].plot(figsize=(10, 6))
 plt.xlabel('Boosting Round')
 plt.ylabel('RMSE')
-plt.title('XGBoost CV RMSE')
+plt.title('XGBoost Cross-validation RMSE')
 plt.show()
 
-# Mean Absolute Error (MAE): 9.128461565290198
-# Root Mean Squared Error (RMSE): 22.163604236348917
-# Indices of residual outliers: [9]
+model.save_model('xgb_model.json')
 
 # # #############################################################################
 # # #############################################################################
-# # CatBoost model
+# CatBoost model
 
 # from catboost import CatBoostRegressor
-# from sklearn.model_selection import cross_val_score
 
 # model = CatBoostRegressor(
 #     iterations=1200,  # Number of boosting rounds
@@ -177,13 +202,11 @@ plt.show()
 # )
 
 # # Train the model
-# model.fit(X_train_scaled, y_log)
+# model.fit(X_train_scaled, y_train_log)
 # model.get_feature_importance()
 
 # # Mean Absolute Error (MAE): 10.878789415160254
 # # Root Mean Squared Error (RMSE): 16.674119917892348
-# cv_scores = cross_val_score(model, X_train_scaled, y_log, cv=5, scoring='neg_mean_absolute_error')
-# print(f"Cross-Validation MAE: {-np.mean(cv_scores)}")
 
 # model.save_model('catboost_model.bin')
 
@@ -192,28 +215,57 @@ plt.show()
 
 
 # # Evaluate the model
-y_pred_original = np.expm1(model.predict(X_val_scaled)).ravel()  # Predicted unit price in original scale
-y_val_original = np.expm1(y_val_log).ravel()  # Actual unit price in original scale
+y_val = np.expm1(y_val_log).ravel()  # Actual unit price in original scale
 
-mae = mean_absolute_error(y_val_original, y_pred_original)
-rmse = np.sqrt(mean_squared_error(y_val_original, y_pred_original))
+y_pred_val = np.expm1(model.predict(X_val_scaled)).ravel()  # Predicted unit price in original scale
+y_pred_train = np.expm1(model.predict(X_train_scaled)).ravel()
+y_pred_test = np.expm1(model.predict(X_test_scaled)).ravel()
+
+# ######################################################################
+# ######################################################################
+# # What's below doesn't work with Keras, only for Scikit-learn
+
+# rmse_val = np.sqrt(mean_squared_error(y_val, y_pred_val))
+# rmse_train = np.sqrt(mean_squared_error(y_train, y_pred_train))
+# rmse_test = np.sqrt(mean_squared_error(y_test, y_pred_test))
+
+# mae_val = mean_absolute_error(y_val, y_pred_val)
+# mae_train = mean_absolute_error(y_train, y_pred_train)
+# mae_test = mean_absolute_error(y_test, y_pred_test)
+
+# try:
+#     cv_scores = cross_val_score(model, X_train_scaled, y_train_log, cv=3, scoring='neg_mean_absolute_error', error_score='raise')
+#     rmse_scores = np.sqrt(-cv_scores)
+#     mean_rmse = np.mean(rmse_scores)
+#     std_rmse = np.std(rmse_scores)
+
+#     print(f"Cross-validation RMSE per fold: {rmse_scores}")
+#     print(f"Mean CV RMSE: {mean_rmse}")
+#     print(f"Standard deviation of CV RMSE: {std_rmse}")
+
+# except ValueError as e:
+#     print(f"Cross-validation failed: {e}")
+# ######################################################################
+# ######################################################################
 
 # Plot Actual vs Predicted prices
 plt.figure(figsize=(8, 6))
-plt.scatter(y_val_original, y_pred_original, alpha=0.6, c='blue')
-plt.plot([y_val_original.min(), y_val_original.max()], [y_val_original.min(), y_val_original.max()], 'k--', lw=2)
+plt.scatter(y_val, y_pred_val, alpha=0.6, c='blue')
+plt.plot([y_val.min(), y_val.max()], [y_val.min(), y_val.max()], 'k--', lw=2)
+plt.scatter(y_train, y_pred_train, alpha=0.6, c='red')
+plt.plot([y_train.min(), y_pred_train.max()], [y_train.min(), y_train.max()], 'k--', lw=2)
+plt.scatter(y_test, y_pred_test, alpha=0.6, c='black')
+plt.plot([y_test.min(), y_test.max()], [y_test.min(), y_test.max()], 'k--', lw=2)
 plt.xlabel('Actual Unit Price')
 plt.ylabel('Predicted Unit Price')
 plt.title('Actual vs Predicted Unit Price')
 plt.grid(True)
 plt.show()
 
-print(f"Mean Absolute Error (MAE): {mae}")
-print(f"Root Mean Squared Error (RMSE): {rmse}")
 
 # #############################################################################
 # Residuals Calculation
-residuals = np.abs(y_val_original - y_pred_original)  # Calculate absolute residuals
+residuals = np.abs(y_val - y_pred_val)  # Calculate absolute residuals
 
 # You can plot the residuals to visually inspect them
 plt.figure(figsize=(8, 6))
@@ -234,7 +286,7 @@ print(f"Indices of residual outliers: {outlier_indices}")
 
 # You can remove outliers based on the residuals (if needed)
 X_val_no_outliers = X_val_scaled[~outliers]  # Remove outliers from validation set features
-y_val_no_outliers = y_val_original[~outliers]  # Remove outliers from the target variable
+y_val_no_outliers = y_val[~outliers]  # Remove outliers from the target variable
 
 # Optionally, you can evaluate the model performance without outliers
 # model.evaluate(X_val_no_outliers, y_val_no_outliers) # Only for Keras models
--- predict.py
+++ predict.py
@@ -1,45 +1,75 @@
+import os
 import tensorflow as tf # Keras
 from catboost import CatBoostRegressor # CatBoost
 import xgboost as xgb # XGBoost
 import numpy as np
+import json
 import joblib
 import utils
 
+product_path = "./Products"
+
 # Load the data
-DATA = utils.read_json("./Products/해바라기씨유.json")
-review_ratios = [obj['REVIEW_RATIO'] for obj in DATA]
-review_counts = [obj['REVIEW_RATIO'] for obj in DATA]
+def load_files(PRODUCT_NAME):
+    data = []
+    for root, _, files in os.walk(product_path):
+        for file in files:
+            file_name = os.path.splitext(file)[0]  # Remove file extension
 
-FIXED_ITEM_COUNT = 4
+            if PRODUCT_NAME in file_name:
+                try:
+                    with open(product_path + '/' + file_name + '.json', 'r', encoding='utf-8') as f:  # Open the file
+                        data = json.load(f)  # Load JSON data
+                except json.JSONDecodeError:
+                    print(f"Error decoding JSON in file: {file_name}")
+    return data
 
-top_10_percentile_r = np.percentile(review_ratios, 90)
-top_10_percentile_c = np.percentile(review_counts, 90)
-top_10_values_r = [value for value in review_ratios if value >= top_10_percentile_r]
-top_10_values_c = [value for value in review_ratios if value >= top_10_percentile_c]
-average_top_10_r  = np.mean(top_10_values_r)
-average_top_10_c  = np.mean(top_10_values_c)
 
-# Load the trained model
-keras_model = tf.keras.models.load_model("optimal_price_model.keras")
-loaded_model = CatBoostRegressor()
-catboost_model = loaded_model.load_model('catboost_model.bin')
+def UserInt():
+    # Predefined
+    PRODUCT_NAME = input("Please enter product name:") or "해바라기기"
+    DATA = load_files(PRODUCT_NAME)
+    FIXED_ITEM_COUNT = int(input("Please enter item count:")) or 4
+    QUANTITY = int(input("Please enter quantity:")) or 1000
+    MODEL_INPUT = input("Please enter model type: ") or "XGB"
+    MODEL_TYPE = None
 
-scaler_X = joblib.load('scaler_x.pkl')
+    # Caculations
+    review_ratios = [obj['REVIEW_RATIO'] for obj in DATA]
+    review_counts = [obj['REVIEW_COUNT'] for obj in DATA]
+    inv_quantity = 1 / QUANTITY
+    log_quantity = np.log1p(QUANTITY)
+    total_quantity = FIXED_ITEM_COUNT * QUANTITY
 
-quantity = 5000 # change this!
-inv_quantity = 1 / quantity
-log_quantity = np.log1p(quantity)
-total_quantity = FIXED_ITEM_COUNT * quantity
+    # Filtering outliers
+    top_10_percentile_r = np.percentile(review_ratios, 90)
+    top_10_percentile_c = np.percentile(review_counts, 90)
+    top_10_values_r = [value for value in review_ratios if value >= top_10_percentile_r]
+    top_10_values_c = [value for value in review_ratios if value >= top_10_percentile_c]
+    average_top_10_r  = np.mean(top_10_values_r)
+    average_top_10_c  = np.mean(top_10_values_c)
 
-X_new = np.array([FIXED_ITEM_COUNT, quantity, average_top_10_r, average_top_10_c, inv_quantity, log_quantity, total_quantity]).reshape(1, -1)
+    if MODEL_INPUT == 'XGB':
+        xgb_loaded = xgb.XGBRegressor()
+        xgb_loaded.load_model('xgb_model.json')
+        MODEL_TYPE = xgb_loaded
+    if MODEL_INPUT == 'CAT':
+        catBoost_loaded = CatBoostRegressor()
+        MODEL_TYPE = catBoost_loaded.load_model('catboost_model.bin')
+    if MODEL_INPUT == 'KERAS':
+        MODEL_TYPE = tf.keras.models.load_model("optimal_price_model.keras")
 
-X_new_scaled = scaler_X.transform(X_new)
+    scaler_X = joblib.load('scaler_x.pkl')
+    X_new = np.array([FIXED_ITEM_COUNT, QUANTITY, average_top_10_r, average_top_10_c, inv_quantity, log_quantity, total_quantity]).reshape(1, -1)
+    X_new_scaled = scaler_X.transform(X_new)
 
-# Make predictions
-# y_pred_log = model.predict(X_new_scaled)
-y_pred_log = catboost_model.predict(X_new_scaled)
+    # Make predictions
+    # y_pred_log = model.predict(X_new_scaled)
+    y_pred_log = MODEL_TYPE.predict(X_new_scaled)
 
-y_pred = np.expm1(y_pred_log) 
+    y_pred = np.expm1(y_pred_log) 
+    # Output the predictions
+    print("Predicted Optimal Price:", y_pred) 
 
-# Output the predictions
-print("Predicted Optimal Price:", y_pred) 
+
+UserInt()
